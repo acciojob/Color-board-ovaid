8f8d7740962899f96102ff483b357cdeadbe4aee
RAMEEZ RAJA <84043654+ovaid@users.noreply.github.com>
2023-04-28 01:07:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const container = document.querySelector('.container');
+
+// Generate 800 squares dynamically
+for (let i = 0; i < 800; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  square.addEventListener('mouseenter', () => {
+    square.style.backgroundColor = '#ccc';
+  });
+  square.addEventListener('mouseleave', () => {
+    setTimeout(() => {
+      square.style.backgroundColor = '#eee';
+    }, 1000);
+  });
+  container.appendChild(square);
+}
